ca925a5089a9aed679554e0a6a770ecfed486e30
AKIRA SAKAGUCHI <akira.sakaguchi.xt@hitachi-systems.com>
2016-02-25 10:58:38 +0900

    sysout add

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world.");
 		System.out.println("Hello, world2.");
+		System.out.println("Hello, world3.");
 	}
 
 }
