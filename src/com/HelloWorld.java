7858acdf7b4770c0ae60732f8e8036540620e5e1
AKIRA SAKAGUCHI <akira.sakaguchi.xt@hitachi-systems.com>
2016-03-01 13:57:35 +0900

    dell out

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -3,11 +3,6 @@ package com;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world.");
-		System.out.println("Hello, world2.");
-		System.out.println("Hello, world3.");
-		System.out.println("Hello, world5.");
-		System.out.println("Hello, world20.");
 	}
 
 }
